50bafe13dc1a2ebac07daafb82ab354991c3771a
NASR <c7nasr@gmail.com>
2021-04-25 15:38:50 +0200

    HomeScreen UI

--- eRemote/App.js
+++ eRemote/App.js
@@ -6,12 +6,14 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import InfoScreen from './navigation/info.screen';
 import HomeScreen from './navigation/home.screen';
+import {StatusBar} from 'react-native';
 const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar animated={true} translucent backgroundColor="transparent" />
       {/* // UnAuthed Stack */}
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home" headerMode="none">
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- eRemote/navigation/home.screen.js
+++ eRemote/navigation/home.screen.js
@@ -1,10 +1,36 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, Button} from 'react-native-ui-lib';
+
 import React from 'react';
+import {Linking} from 'react-native';
 
 function HomeScreen() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>HomeScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+      }}>
+      <Image
+        source={require('./../assets/logo.png')}
+        width={200}
+        height={150}
+      />
+      <Text text20H dark10 center>
+        nxxx-xxxx-xxxx
+      </Text>
+      <Text text70H dark30 center>
+        Enter Code Above in the PC Client to Start Authentication Process
+      </Text>
+      <Button outline label="I've entered the Code on PC Client" marginT-10 />
+      <Text
+        text80H
+        green10
+        style={{marginTop: 10}}
+        onPress={() => Linking.openURL('https://eremote.tech')}>
+        If you don't have the PC Client click here
+      </Text>
     </View>
   );
 }
