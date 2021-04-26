93b6e4a360992f85a6e6ba27de42460483a571e6
NASR <c7nasr@gmail.com>
2021-04-26 23:41:16 +0200

    MediaUI

--- eRemote/navigation/control/media.screen.js
+++ eRemote/navigation/control/media.screen.js
@@ -1,10 +1,108 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image, TouchableOpacity, View} from 'react-native';
+import {Text, Colors, Slider} from 'react-native-ui-lib';
 
 export default function MediaScreen() {
   return (
-    <View>
-      <Text>MediaScreen</Text>
+    <View style={{flex: 1, backgroundColor: Colors.green10}}>
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 25,
+        }}>
+        <Text text50 style={{color: 'white'}}>
+          Media Control
+        </Text>
+        <Text text70 style={{color: 'white'}}>
+          Control system volume, mute, control playing media with one click.
+        </Text>
+
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            backgroundColor: '#fff',
+            marginTop: 50,
+          }}>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#fff',
+              height: 40,
+              borderRadius: 50,
+              padding: 5,
+              marginHorizontal: 5,
+            }}>
+            <Image
+              style={{
+                width: 32,
+                height: 32,
+                tintColor: Colors.blue20,
+              }}
+              source={require('../../assets/icons/media/backward.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#fff',
+              height: 40,
+              borderRadius: 50,
+              padding: 5,
+              marginHorizontal: 5,
+            }}>
+            <Image
+              style={{
+                width: 32,
+                height: 32,
+                tintColor: Colors.blue20,
+              }}
+              source={require('../../assets/icons/media/pause_play.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#fff',
+              height: 40,
+              borderRadius: 50,
+              padding: 5,
+              marginHorizontal: 5,
+            }}>
+            <Image
+              style={{
+                width: 32,
+                height: 32,
+                tintColor: Colors.blue20,
+              }}
+              source={require('../../assets/icons/media/forward.png')}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#fff',
+              height: 40,
+              borderRadius: 50,
+              padding: 5,
+            }}>
+            <Image
+              style={{
+                width: 32,
+                height: 32,
+                tintColor: Colors.blue20,
+              }}
+              source={require('../../assets/icons/media/mute.png')}
+            />
+          </TouchableOpacity>
+        </View>
+        <Slider
+          onValueChange={e => console.log(e)}
+          value={58}
+          minimumValue={0}
+          maximumValue={100}
+          containerStyle={{marginTop: 80}}
+          step={1}
+        />
+      </View>
     </View>
   );
 }
