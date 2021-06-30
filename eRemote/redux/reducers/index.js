d12a2c92e1fde76e4b5ef449ebe8133dc581b415
NASR <c7nasr@gmail.com>
2021-06-30 15:09:23 +0200

    Update index.js

--- eRemote/redux/reducers/index.js
+++ eRemote/redux/reducers/index.js
@@ -5,12 +5,38 @@ import Toast from './Toast';
 import PC from './PC';
 import Socket from './Socket';
 import Reports from './Reports';
+import {persistReducer} from 'redux-persist';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import ScreenAndCamera from './ScreenAndCamera';
 
-export default combineReducers({
-  App,
-  Auth,
-  Toast,
-  PC,
-  Socket,
-  Reports,
-});
+const AppPersistConfig = {
+  key: 'App',
+  storage: EncryptedStorage,
+  keyPrefix: '',
+};
+const PCPersistConfig = {
+  key: 'PC',
+  storage: EncryptedStorage,
+  blacklist: ['is_connected'],
+};
+
+const persistConfig = {
+  key: 'root',
+  storage: EncryptedStorage,
+  blacklist: [''],
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
+    Auth,
+    App: persistReducer(AppPersistConfig, App),
+    PC: persistReducer(PCPersistConfig, PC),
+    Toast,
+    Socket,
+    Reports,
+    ScreenAndCamera,
+  }),
+);
+
+export default persistedReducer;
