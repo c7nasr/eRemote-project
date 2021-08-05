6846f463e6779a4415ba5dd56607a0b4aa46a144
NASR <c7nasr@gmail.com>
2021-08-05 19:34:59 +0200

    Update time.service.js

--- web2.0/services/time.service.js
+++ web2.0/services/time.service.js
@@ -1,11 +1,17 @@
 const moment = require("moment");
 
-exports.formatTime = (date) => {
-  return moment(date).format("DD MMMM YYYY@h:mm:ss a");
+exports.formatTime = (date, format = "DD MMMM YYYY@h:mm:ss a") => {
+  return moment(date).format(format);
+};
+exports.formatTimestamp = (date, format = "DD MMMM YYYY@h:mm:ss a") => {
+  var timestamp = date / 1;
+  var date_js = new Date(timestamp);
+  return this.formatTime(date_js, format);
 };
 exports.formatTimeAgo = (date) => {
   return moment(date).fromNow();
 };
+
 exports.capitalizeFirstLetter = (string) => {
   if (string) return string[0].toUpperCase() + string.slice(1).toLowerCase();
 
